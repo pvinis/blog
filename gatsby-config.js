17d3c489a9934f46367fec25d549688833661e70
Pavlos Vinieratos <pvinis@gmail.com>
2019-03-22 16:03:10 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Where the wild codes are`,
+    author: `Pavlos Vinieratos`,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://pvin.is`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `pvinis`,
+      github: `pvinis`,
     },
   },
   plugins: [
