366f045d695aadb43d247cc56949cf628f330d61
Pavlos Vinieratos <pvinis@gmail.com>
2019-06-08 08:35:19 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,86 +1,86 @@
 module.exports = {
-  siteMetadata: {
-    title: `Where the wild codes are`,
-    author: `Pavlos Vinieratos`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://pvin.is`,
-    social: {
-      twitter: `pvinis`,
-      github: `pvinis`,
+    siteMetadata: {
+        title: 'Purple Royale',
+        author: 'Pavlos Vinieratos',
+        description: 'The website and blog of Pavlos, where he writes about code and codes about stuff.',
+        siteUrl: 'https://pvin.is',
+        social: {
+            twitter: 'pvinis',
+            github: 'pvinis',
+        },
     },
-  },
-  plugins: [
-      {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-138234470-1",
-        head: true,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-google-analytics',
             options: {
-              maxWidth: 590,
+                trackingId: 'UA-141725733-1',
+                head: true,
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+                path: `${__dirname}/content/blog`,
+                name: 'blog',
             },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/content/assets`,
+                name: 'assets',
+            },
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 590,
+                        },
+                    },
+                    {
+                        resolve: 'gatsby-remark-responsive-iframe',
+                        options: {
+                            wrapperStyle: 'margin-bottom: 1.0725rem',
+                        },
+                    },
+                    'gatsby-remark-prismjs',
+                    'gatsby-remark-copy-linked-files',
+                    'gatsby-remark-smartypants',
+                ],
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            },
+        },
+        'gatsby-plugin-feed',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Gatsby Starter Blog',
+                short_name: 'GatsbyJS',
+                start_url: '/',
+                background_color: '#ffffff',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                icon: 'content/assets/gatsby-icon.png',
+            },
+        },
+        'gatsby-plugin-offline',
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography',
+            },
+        },
+    ],
 }
