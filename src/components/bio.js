704dff4b81c54254ea9266cf313287162bfbbcdd
Pavlos Vinieratos <pvinis@gmail.com>
2019-06-08 09:25:50 +0200

    more

--- src/components/bio.js
+++ src/components/bio.js
@@ -39,11 +39,10 @@ function Bio() {
                             }}
                         />
                         <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+                            Written by <strong>{author}</strong> who currently lives and works in Rotterdam building useful things.
                             {' '}
                             <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                                You should follow him on Twitter
                             </a>
                         </p>
                     </div>
--- src/components/layout.js
+++ src/components/layout.js
@@ -63,9 +63,15 @@ class Layout extends React.Component {
                 <header>{header}</header>
                 <main>{children}</main>
                 <footer>
-          © {new Date().getFullYear()}, Built with
+                    © {new Date().getFullYear()}, Built by
+                    {' '}
+                    <a href="https://www.github.com/pvinis">pvinis</a>
+                    {' '}
+                    using
                     {' '}
                     <a href="https://www.gatsbyjs.org">Gatsby</a>
+                    {' '}
+                    with 💜
                 </footer>
             </div>
         )
