e842838edf0cf717325b14253961e841f2c01178
Pavlos Vinieratos <pvinis@gmail.com>
2021-01-25 13:50:07 +0400

    try

--- pages/_document.js
+++ pages/_document.js
@@ -2,13 +2,21 @@ import Document, { Head, Main, NextScript, Html } from "next/document";
 
 import { getSiteMetaData } from "@utils/helpers";
 
+const GOOGLE_ANALYTICS_ID = 'UA-141718871-1'
+
 export default class MyDocument extends Document {
   render() {
     const siteMetadata = getSiteMetaData();
 
     return (
       <Html lang={siteMetadata.language}>
-        <Head />
+        <Head>
+		<script
+            dangerouslySetInnerHTML={{
+              __html: `${GOOGLE_ANALYTICS_ID}`
+            }}
+          />
+		</Head>
         <body>
           <Main />
           <NextScript />
